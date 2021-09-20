1478004d1c7111bf1a534394d4f301395a17e822
Kollawach Satiansareesuk <kollawach.s@ku.th>
2021-09-20 12:09:39 +0700

    initialize project

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BootstrapStyleSheet from 'react-native-bootstrap-styles';
+
+const BODY_COLOR = '#000022', TEXT_PRIMARY = '#882288';
+
+const constants = {
+  BODY_COLOR, TEXT_PRIMARY,
+};
+
+const bootstrapStyleSheet = new BootstrapStyleSheet(constants);
+const { styles: s, constants: c } = bootstrapStyleSheet;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Estate appraisal system</Text>
+      </View>
+      <View style={s.body}>
+        <Text style={[s.text, s.textPrimary]}>Initialize the project!</Text>
+        <Text>Subject : System Analysis</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +33,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor:'red',
+    padding:20,
+  },
+  boldText:{
+    fontWeight: 'bold'
+  },
+  body:{
+    backgroundColor:'pink',
+    alignItems: 'center',
+    padding:20,
   },
 });
